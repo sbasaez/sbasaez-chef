15e279cf1d02e2e852dcd0d6b09534f0c680767b
sergio <basaez163@gmail.com>
2017-11-26 22:25:24 +0000

    apache basic

---
+++ cookbooks/apache/recipes/default.rb
@@ -0,0 +1,18 @@
+#
+# Cookbook:: apache
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
+#install apache 
+
+package 'apache2' do
+	package_name 'httpd'
+	action :install
+end
+
+service 'apache' do
+	service_name 'httpd'
+	action [:start, :enable]
+end
+
+

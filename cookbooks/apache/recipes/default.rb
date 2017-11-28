ae8a098b29b1fbc193a738d445f2f7ecfebef115
sergio <basaez163@gmail.com>
2017-11-28 03:03:42 +0000

    receta nueva

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -5,15 +5,23 @@
 # Copyright:: 2017, The Authors, All Rights Reserved.
 #install apache 
 
+
+if node[platform_family] == 'rhel'
+	package = 'httpd'
+elsif node[platform_family] == 'debian'
+	package = 'apache2'
+
+end
+
+
 package 'apache2' do
-	package_name 'httpd'
+	package_name package
 	action :install
 end
 
 service 'apache' do
-	service_name 'httpd'
+	service_name package
 	action [:start, :enable]
 end
 
-include_recipe 'apache:: websites'
 
